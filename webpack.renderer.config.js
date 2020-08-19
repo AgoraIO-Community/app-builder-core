e9f68129dfd45135c1433e3911be094a8927dae3
technophilic <svineth.face@gmail.com>
2020-08-19 15:43:38 +0530

    electron fix

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,12 +4,8 @@ const {merge} = require('webpack-merge');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV === 'development';
 const path = require('path');
-const { platform } = require('os');
 
 module.exports = merge(commons, electronCommons, {
-  externals:{
-    "agora-electron-sdk":'commonjs2 agora-electron-sdk'
-  },
   plugins: [
     isDevelopment &&
       new ReactRefreshWebpackPlugin({
@@ -19,11 +15,22 @@ module.exports = merge(commons, electronCommons, {
       }),
   ].filter(Boolean),
   resolve: {
-    extensions: ['.electron.tsx', '.electron.ts', '.tsx', '.ts', '.jsx', '.js'],
+    extensions: [
+      '.electron.tsx',
+      '.electron.ts',
+      '.tsx',
+      '.ts',
+      '.jsx',
+      '.js',
+      '.node',
+    ],
     alias: {
       'react-native$': 'react-native-web',
       'agora-react-native-rtm': path.join(__dirname, 'bridge/rtm/web'),
-      'react-native-agora': (process.env.PLATFORM ==='linux')?path.join(__dirname, 'bridge/rtc/web') : path.join(__dirname, 'bridge/rtc/electron'),
+      'react-native-agora':
+        process.env.PLATFORM === 'linux'
+          ? path.join(__dirname, 'bridge/rtc/web')
+          : path.join(__dirname, 'bridge/rtc/electron'),
     },
   },
 });
