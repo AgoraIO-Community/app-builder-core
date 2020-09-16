6cc0d0f9bd45965d30fc551b4c2f5bb914dd08cf
technophilic <svineth.face@gmail.com>
2020-09-16 18:13:37 +0530

    added config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 // This file is read only by react native for IOS & Android. Doesn't apply to electron, Web targets
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['preval'],
 };
