69cd779e60c1eeb80a4d53966802b660d17653dd
technophilic <svineth.face@gmail.com>
2020-09-25 13:24:49 +0530

    dist removal

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  globals: {
-    $config: true
-  }
-};
