6e33acdabfaa02f298574a7c30ec3cc151775e09
Nitesh Tiwari <nitte.tiwari1993@gmail.com>
2021-06-09 10:51:05 +0530

    dev protocol handler for windows

--- template/electron/main/index.js
+++ template/electron/main/index.js
@@ -92,20 +92,20 @@ const createWindow = () => {
 };
 
 let deeplinkingUrl;
-if (!app.isDefaultProtocolClient('myapp')) {
-  // Define custom protocol handler. Deep linking works on packaged versions of the application!
-  app.setAsDefaultProtocolClient('appbuilder');
-}
-// if (isDevelopment && process.platform === 'win32') {
-//   // Set the path of electron.exe and your app.
-//   // These two additional parameters are only available on windows.
-//   // Setting this is required to get this working in dev mode.
-//   app.setAsDefaultProtocolClient('appbuilder', process.execPath, [
-//     resolve(process.argv[1])
-//   ]);
-// } else {
+// if (!app.isDefaultProtocolClient('myapp')) {
+//   // Define custom protocol handler. Deep linking works on packaged versions of the application!
 //   app.setAsDefaultProtocolClient('appbuilder');
 // }
+if (isDevelopment && process.platform === 'win32') {
+  // Set the path of electron.exe and your app.
+  // These two additional parameters are only available on windows.
+  // Setting this is required to get this working in dev mode.
+  app.setAsDefaultProtocolClient('appbuilder', process.execPath, [
+    path.resolve(process.argv[1])
+  ]);
+} else {
+  app.setAsDefaultProtocolClient('appbuilder');
+}
 
 const gotTheLock = app.requestSingleInstanceLock();
 if (gotTheLock) {
