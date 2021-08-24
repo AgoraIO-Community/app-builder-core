52dbd24b73159ecff2adb2e9d284b951da7462d4
Adictya <aditya.26101@gmail.com>
2021-08-24 17:19:53 +0530

    added licenses

--- template/src/components/PreventNavigation.native.tsx
+++ template/src/components/PreventNavigation.native.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import {Prompt} from './Router.native'
+/*
+********************************************
+ Copyright © 2021 Agora Lab, Inc., all rights reserved.
+ AppBuilder and all associated components, source code, APIs, services, and documentation 
+ (the “Materials”) are owned by Agora Lab, Inc. and its licensors. The Materials may not be 
+ accessed, used, modified, or distributed for any purpose without a license from Agora Lab, Inc.  
+ Use without a license or in violation of any license terms and conditions (including use for 
+ any purpose competitive to Agora Lab, Inc.’s business) is strictly prohibited. For more 
+ information visit https://appbuilder.agora.io. 
+*********************************************
+*/
+import React from "react";
+import { Prompt } from "./Router.native";
 
 interface PreventNavigationProps {
-  message: string
+  message: string;
 }
 
-const PreventNavigation: React.FC<PreventNavigationProps> = ({message}) => {
-  return <Prompt message={message}/>;
+const PreventNavigation: React.FC<PreventNavigationProps> = ({ message }) => {
+  return <Prompt message={message} />;
 };
 
 export default PreventNavigation;
