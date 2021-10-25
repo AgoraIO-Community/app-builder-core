139f9a9a3a2cad0ebdb075ad483daa5193a83249
Supriya Adep <supriya@agora.io>
2021-10-25 15:17:44 +0530

    update enum changes

--- template/src/components/RTMEvents.tsx
+++ template/src/components/RTMEvents.tsx
@@ -10,12 +10,12 @@
 *********************************************
 */
 
-import {mChannelType, messageEventInterface} from './ChatContext';
+import {messageChannelType, messageEventInterface} from './ChatContext';
 
 const RTM_ERROR_TEMPLATE = 'RTMError:';
 
 type eventsMapInterface = {
-  [key in mChannelType]: Record<string, any>;
+  [key in messageChannelType]: Record<string, any>;
 };
 
 type errorObjectInterface = {
@@ -26,27 +26,31 @@ type errorObjectInterface = {
 type eventKeyType = keyof eventsMapInterface;
 
 const eventsMap: eventsMapInterface = {
-  [mChannelType.Private]: new Map<string, any>(),
-  [mChannelType.Public]: new Map<string, any>(),
+  [messageChannelType.Private]: new Map<string, any>(),
+  [messageChannelType.Public]: new Map<string, any>(),
 };
 
 export interface rtmEventsInterface {
-  on: (mChannel: mChannelType, evtName: string, callback: any) => void;
+  on: (
+    messageChannel: messageChannelType,
+    evtName: string,
+    callback: any,
+  ) => void;
   emit: (
-    mChannel: mChannelType,
+    messageChannel: messageChannelType,
     data: messageEventInterface | any | null,
     error?: errorObjectInterface | null | undefined,
   ) => void;
-  off: (mChannel: mChannelType, evtName: string) => void;
+  off: (messageChannel: messageChannelType, evtName: string) => void;
   destroyAll: () => void;
 }
 
 const events: rtmEventsInterface = {
-  on: (mChannel: mChannelType, evtName: string, callback: any) => {
-    eventsMap[mChannel].set(evtName, callback);
+  on: (messageChannel: messageChannelType, evtName: string, callback: any) => {
+    eventsMap[messageChannel].set(evtName, callback);
   },
   emit: (
-    mChannel: mChannelType,
+    messageChannel: messageChannelType,
     data: messageEventInterface | any | null,
     error: errorObjectInterface | null | undefined,
   ) => {
@@ -55,18 +59,18 @@ const events: rtmEventsInterface = {
       let err = new Error(`${RTM_ERROR_TEMPLATE}: ${error.msg}`);
       err.stack += '\nCaused by: ' + error.cause;
 
-      for (const [key] of eventsMap[mChannel].entries()) {
-        eventsMap[mChannel].get(`${key}`)(null, err);
+      for (const [key] of eventsMap[messageChannel].entries()) {
+        eventsMap[messageChannel].get(`${key}`)(null, err);
       }
       return;
     }
     // Handle success, return data in callback
-    for (const [key] of eventsMap[mChannel].entries()) {
-      eventsMap[mChannel].get(`${key}`)(data, null);
+    for (const [key] of eventsMap[messageChannel].entries()) {
+      eventsMap[messageChannel].get(`${key}`)(data, null);
     }
   },
-  off: (mChannel: mChannelType, evtName: string) => {
-    eventsMap[mChannel].delete(evtName);
+  off: (messageChannel: messageChannelType, evtName: string) => {
+    eventsMap[messageChannel].delete(evtName);
   },
   destroyAll: () => {
     for (const key of Object.keys(eventsMap) as Array<eventKeyType>) {
