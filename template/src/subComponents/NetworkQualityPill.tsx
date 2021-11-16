db2fa2ca6c070ca20ba0206b81f7df9b621ed4bd
adictya <aditya.26101@gmail.com>
2021-11-16 16:11:50 +0530

    added network pill sub component

---
+++ template/src/subComponents/NetworkQualityPill.tsx
@@ -0,0 +1,143 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Image,
+  Text,
+  Pressable,
+  Platform,
+  StyleSheet,
+  ViewStyle,
+  StyleProp,
+} from 'react-native';
+import {networkIconsObject} from '../components/NetworkQualityContext';
+
+export const NetworkQualityPill = ({
+  networkStat,
+  primaryColor,
+  small,
+  rootStyle,
+}: {
+  networkStat: number;
+  primaryColor: any;
+  small?: boolean;
+  rootStyle?: StyleProp<ViewStyle>;
+}) => {
+  const [networkTextVisible, setNetworkTextVisible] = useState(false);
+
+  return (
+    <View
+      style={[
+        style.networkPill,
+        {
+          opacity: networkTextVisible ? 1 : 0.5,
+        },
+        rootStyle,
+      ]}>
+      <PlatformSpecificWrapper {...{networkTextVisible, setNetworkTextVisible}}>
+        <View style={[style.networkIndicatorBackdrop]}>
+          <Image
+            source={{
+              uri: networkIconsObject[networkStat || 0].icon,
+            }}
+            style={[
+              style.networkIcon,
+              {
+                tintColor:
+                  networkIconsObject[networkStat || 0].tint === 'primary'
+                    ? primaryColor
+                    : networkIconsObject[networkStat || 0].tint,
+              },
+            ]}
+            resizeMode={'contain'}
+          />
+        </View>
+        {networkTextVisible && (
+          <Text
+            textBreakStrategy={'simple'}
+            style={[style.networkPillText, {fontSize: small ? 14 : 15}]}>
+            {networkIconsObject[networkStat || 0].text}
+          </Text>
+        )}
+      </PlatformSpecificWrapper>
+    </View>
+  );
+};
+
+const PlatformSpecificWrapper = ({
+  networkTextVisible,
+  setNetworkTextVisible,
+  children,
+}: any) => {
+  return Platform.OS !== 'web' ? (
+    <Pressable
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+      onPress={() => {
+        networkTextVisible
+          ? setNetworkTextVisible(false)
+          : setNetworkTextVisible(true);
+      }}
+    >
+      {children}
+    </Pressable>
+  ) : (
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+      onMouseEnter={() => {
+        setNetworkTextVisible(true);
+      }}
+      onMouseLeave={() => {
+        setNetworkTextVisible(false);
+      }}>
+      {children}
+    </div>
+  );
+};
+
+const style = StyleSheet.create({
+  networkPill: {
+    height: 30,
+    backgroundColor: $config.SECONDARY_FONT_COLOR + 'bb',
+    position: 'absolute',
+    zIndex: 2,
+    paddingHorizontal: 0,
+    borderRadius: 15,
+  },
+  networkPillContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  networkPillText: {
+    color: $config.PRIMARY_FONT_COLOR,
+    lineHeight: 30,
+    fontSize: 15,
+    fontWeight: '600',
+    marginLeft: 5,
+    marginRight: 15,
+  },
+  networkIcon: {
+    width: '80%',
+    height: '80%',
+    alignSelf: 'center',
+  },
+  networkIndicatorBackdrop: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+    backgroundColor: $config.SECONDARY_FONT_COLOR,
+    justifyContent: 'center',
+  },
+});
