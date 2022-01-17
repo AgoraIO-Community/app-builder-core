a0ce0b6d819a1c937a7e5b952caa490d467b8174
HariharanIT <ghariharanit@gmail.com>
2022-01-17 13:21:38 +0530

    Fixed code formatting

--- template/src/pages/VideoCall.tsx
+++ template/src/pages/VideoCall.tsx
@@ -109,19 +109,19 @@ const NotificationControl = ({children, chatDisplayed, setSidePanel}) => {
 
   React.useEffect(() => {
     const showMessageNotification = (data: any) => {
-		if(data.type === messageActionType.Normal){
-			const {uid, msg} = data;
-			Toast.show({
-				type: 'success',
-				text1: msg.length > 50 ? msg.slice(0, 50) + '...' : msg,
-				text2: userList[uid]?.name ? 'From: ' + userList[uid]?.name : '',
-				visibilityTime: 1000,
-				onPress: () => {
-				setSidePanel(SidePanelType.Chat);
-				setLastCheckedPublicState(messageStore.length);
-				},
-			});
-		}
+			if(data.type === messageActionType.Normal){
+				const {uid, msg} = data;
+				Toast.show({
+					type: 'success',
+					text1: msg.length > 50 ? msg.slice(0, 50) + '...' : msg,
+					text2: userList[uid]?.name ? 'From: ' + userList[uid]?.name : '',
+					visibilityTime: 1000,
+					onPress: () => {
+						setSidePanel(SidePanelType.Chat);
+						setLastCheckedPublicState(messageStore.length);
+					},
+				});
+			}
     };
     events.on(
       messageChannelType.Public,
