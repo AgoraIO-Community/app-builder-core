c27f207a0b33575af95b34e5ca6367ff5b7aac12
Supriya Adep <supriya@agora.io>
2022-01-17 17:58:19 +0530

    dev purpose enable true

--- template/src/utils/isLiveStreamingEnabled.tsx
+++ template/src/utils/isLiveStreamingEnabled.tsx
@@ -1,3 +1,9 @@
-const isLiveStreamingEnabled: boolean = !!$config.EVENT_MODE;
+/**
+ * EVENT_MODE: boolean
+ *   true: EVENT (LIVE)
+ *   false: MEETING (RTC)
+ */
 
+// const isLiveStreamingEnabled: boolean = !!$config.EVENT_MODE;
+const isLiveStreamingEnabled: boolean = true; // for deve purpose
 export default isLiveStreamingEnabled;
