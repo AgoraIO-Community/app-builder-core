0f720f74fbf14b24c8539bfa847853816b54ecb1
HariharanIT <ghariharanit@gmail.com>
2022-02-11 10:27:55 +0530

    Removed the unwanted package

--- template/test-fpe/custom-components/PreCall.tsx
+++ template/test-fpe/custom-components/PreCall.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import {concat} from 'lodash';
 const CustomPreCall:React.FC<{}> = () =>{
-
-  const a = ['hello']
-  const b = ['world']
-
-  const c = concat(a,b)
   return(
     <div>
       <h1> PreCall screen custom </h1>
-      <ul>
-      {
-        c.map((i, ) => {
-          return <li  key={i}>{i}</li>
-        })  
-      }
-      </ul>
     </div>
   )
 }
