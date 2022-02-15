bcdb524e29499b04dc150e54ef3137d2ab560b6c
Supriya Adep <supriya@agora.io>
2022-02-16 00:35:13 +0530

    participants view changes

--- template/src/components/participants/AllAudienceParticipants.tsx
+++ template/src/components/participants/AllAudienceParticipants.tsx
@@ -1,56 +1,32 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
-import chatContext from '../ChatContext';
 import RemoteEndCall from '../../subComponents/RemoteEndCall';
 import ParticipantName from '../../components/participants/ParticipantName';
-import {filterObject} from '../../utils/index';
 
 const AllAudienceParticipants = (props: any) => {
-  const {p_style, type, isHost, title} = props;
-  const {userList} = useContext(chatContext);
-
-  const [filteredParticipantsByType, setfilteredParticipantsByType] = useState(
-    {},
-  );
-
-  useEffect(() => {
-    setfilteredParticipantsByType(
-      filterObject(
-        userList,
-        ([k, v]) => v?.type === 0 && v?.role == type && !v.offline,
-      ),
-    );
-  }, [userList]);
+  const {p_style, isHost, participantList} = props;
 
   return (
-    <>
-      <Text style={p_style.subheading}>
-        {title}{' '}
-        <Text style={p_style.count}>
-          ({Object.keys(filteredParticipantsByType).length})
-        </Text>
-      </Text>
-      <View style={p_style.participantContainer}>
-        {Object.keys(filteredParticipantsByType).length == 0 ? (
-          <Text style={p_style.infoText}>No {title}(s) have joined yet</Text>
-        ) : (
-          Object.entries(filteredParticipantsByType).map(
-            ([uid, user]: any, index: number) => (
-              <View style={p_style.participantRow} key={index}>
-                <ParticipantName value={user.name} />
-                {isHost && (
-                  <View style={p_style.participantActionContainer}>
-                    <View style={[p_style.actionBtnIcon]}>
-                      <RemoteEndCall uid={uid} isHost={isHost} />
-                    </View>
+    <View style={p_style.participantContainer}>
+      {Object.keys(participantList).length == 0 ? (
+        <Text style={p_style.infoText}>No one has joined yet</Text>
+      ) : (
+        Object.entries(participantList).map(
+          ([uid, user]: any, index: number) => (
+            <View style={p_style.participantRow} key={index}>
+              <ParticipantName value={user.name} />
+              {isHost && (
+                <View style={p_style.participantActionContainer}>
+                  <View style={[p_style.actionBtnIcon]}>
+                    <RemoteEndCall uid={uid} isHost={isHost} />
                   </View>
-                )}
-              </View>
-            ),
-          )
-        )}
-      </View>
-    </>
+                </View>
+              )}
+            </View>
+          ),
+        )
+      )}
+    </View>
   );
 };
 
---
+++ template/src/components/participants/context/ParticipantContext.tsx
@@ -0,0 +1,69 @@
+/*
+********************************************
+ Copyright © 2021 Agora Lab, Inc., all rights reserved.
+ AppBuilder and all associated components, source code, APIs, services, and documentation 
+ (the “Materials”) are owned by Agora Lab, Inc. and its licensors. The Materials may not be 
+ accessed, used, modified, or distributed for any purpose without a license from Agora Lab, Inc.  
+ Use without a license or in violation of any license terms and conditions (including use for 
+ any purpose competitive to Agora Lab, Inc.’s business) is strictly prohibited. For more 
+ information visit https://appbuilder.agora.io. 
+*********************************************
+*/
+import {createContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+import chatContext from '../../ChatContext';
+import {filterObject} from '../../../utils';
+import {ClientRole} from '../../../../agora-rn-uikit';
+
+interface ParticipantContext {
+  hostList: any;
+  audienceList: any;
+  hostCount: number;
+  audienceCount: number;
+}
+
+const ParticipantContext = createContext(null as unknown as ParticipantContext);
+
+export const ParticipantContextProvider: React.FC = (props: any) => {
+  const [hostCount, setHostCount] = useState(0);
+  const [hostList, setHostList] = useState({});
+  const [audienceList, setAudienceList] = useState({});
+  const [audienceCount, setAudienceCount] = useState(0);
+
+  const {userList} = useContext(chatContext);
+
+  useEffect(() => {
+    if (Object.keys(userList).length !== 0) {
+      const hostList = filterObject(
+        userList,
+        ([k, v]) =>
+          v?.type === 0 && v?.role == ClientRole.Broadcaster && !v.offline,
+      );
+      setHostList(hostList);
+      setHostCount(Object.keys(hostList).length);
+      const audienceList = filterObject(
+        userList,
+        ([k, v]) =>
+          v?.type === 0 && v?.role == ClientRole.Audience && !v.offline,
+      );
+      setAudienceList(audienceList);
+      setAudienceCount(Object.keys(audienceList).length);
+    }
+  }, [userList]);
+
+  return (
+    <ParticipantContext.Provider
+      value={{
+        hostList,
+        audienceList,
+        hostCount,
+        audienceCount,
+      }}>
+      {props.children}
+    </ParticipantContext.Provider>
+  );
+};
+
+export const ParticipantContextConsumer = ParticipantContext.Consumer;
+
+export default ParticipantContext;
