b296115b02603342321f8cf814723a4144e80200
HariharanIT <ghariharanit@gmail.com>
2022-03-02 12:32:42 +0530

    Exported the core context in fpe

--- template/fpe-api/context.ts
+++ template/fpe-api/context.ts
@@ -1,3 +1,14 @@
+/**
+ * Core contexts
+ */
+export {RtcContext, MinUidContext, MaxUidContext, LocalUserContext, PropsContext} from '../agora-rn-uikit';
+export {default as ChatContext} from '../src/components/ChatContext';
+export {default as DeviceContext} from '../src/components/DeviceContext';
+export {default as StorageContext} from '../src/components/StorageContext';
+
+/**
+ * UI contexts
+ */
 export { type PreCallContextInterface, PreCallProvider, usePreCall } from '../src/components/precall/usePreCall';
 export { type VideoCallContextType, VideoCallProvider, useVideoCall } from '../src/pages/VideoCall/useVideoCall';
 export { type ChatUIDataInterface, type privateMsgLastSeen, ChatUIDataProvider, useChatUIData } from '../src/components/useChatUI';
