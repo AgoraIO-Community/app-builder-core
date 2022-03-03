9db46e5fbfda923e7345ab69eab44c3ef89f363f
Supriya Adep <supriya@agora.io>
2022-03-03 14:19:23 +0530

    fixes

--- template/src/utils/index.tsx
+++ template/src/utils/index.tsx
@@ -16,3 +16,33 @@ export function kFormatter(num: number) {
     ? Math.sign(num) * (Math.round(Math.abs(num) / 100) / 10) + 'k'
     : Math.sign(num) * Math.abs(num);
 }
+
+export function numFormatter(num: number) {
+  if (num === null || num === 0) {
+    return '0';
+  }
+  const value = Math.abs(num);
+  const sign = Math.sign(num);
+  let newValue = value;
+  if (value >= 1000) {
+    var suffixes = ['', 'k', 'm', 'b', 't'];
+    var suffixNum = Math.floor(('' + value).length / 3);
+    var shortValue = '';
+    for (var precision = 2; precision >= 1; precision--) {
+      shortValue = parseFloat(
+        (suffixNum != 0
+          ? value / Math.pow(1000, suffixNum)
+          : value
+        ).toPrecision(precision),
+      );
+      var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
+      if (dotLessShortValue.length <= 2) {
+        break;
+      }
+    }
+    if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
+    newValue = shortValue + suffixes[suffixNum];
+  }
+  newValue = sign === -1 ? `-${newValue}` : `${newValue}`;
+  return newValue;
+}
