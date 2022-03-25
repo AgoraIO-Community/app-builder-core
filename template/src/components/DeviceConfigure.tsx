1232add936b89c976cc133c65e55682e11e80ff4
Supriya Adep <supriya@agora.io>
2022-03-25 16:11:06 +0530

    remove dependency from user role

--- template/src/components/DeviceConfigure.tsx
+++ template/src/components/DeviceConfigure.tsx
@@ -9,47 +9,38 @@
  information visit https://appbuilder.agora.io. 
 *********************************************
 */
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext, useEffect, useCallback} from 'react';
 import {RtcContext, ClientRole} from '../../agora-rn-uikit';
 import DeviceContext from './DeviceContext';
 
 interface Props {
   userRole: ClientRole;
 }
+
 const DeviceConfigure: React.FC<Props> = (props: any) => {
-  const {userRole} = props;
   const [selectedCam, setSelectedCam] = useState('');
   const [selectedMic, setSelectedMic] = useState('');
   const [deviceList, setDeviceList] = useState([]);
   const rtc = useContext(RtcContext);
 
-  useEffect(() => {
-    const updateDevices = () => {
-      rtc.RtcEngine.getDevices(function (devices: any) {
-        console.log('set devices');
-        setDeviceList(devices);
-        for (const i in devices) {
-          if (devices[i].kind === 'videoinput') {
-            setSelectedCam(devices[i].deviceId);
-            break;
-          }
+  const refreshDevices = useCallback(async () => {
+    rtc.RtcEngine.getDevices(function (devices: any) {
+      console.log('set devices');
+      setDeviceList(devices);
+      for (const i in devices) {
+        if (devices[i].kind === 'videoinput') {
+          setSelectedCam(devices[i].deviceId);
+          break;
         }
-        for (const i in devices) {
-          if (devices[i].kind === 'audioinput') {
-            setSelectedMic(devices[i].deviceId);
-            break;
-          }
-        }
-      });
-    };
-    if ($config.EVENT_MODE) {
-      if (userRole == ClientRole.Broadcaster) {
-        updateDevices();
       }
-    } else {
-      updateDevices();
-    }
-  }, [userRole]);
+      for (const i in devices) {
+        if (devices[i].kind === 'audioinput') {
+          setSelectedMic(devices[i].deviceId);
+          break;
+        }
+      }
+    });
+  }, []);
 
   useEffect(() => {
     if (selectedCam.length !== 0) {
@@ -72,6 +63,21 @@ const DeviceConfigure: React.FC<Props> = (props: any) => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedMic]);
+
+  useEffect(() => {
+    // See if device is empty
+    const deviceIdIsEmpty = deviceList.find(
+      (device) => device?.deviceId === '',
+    );
+    // If stream exists and deviceIds are empty, check for devices again
+    if (
+      rtc?.RtcEngine?.localStream &&
+      Object.keys(rtc?.RtcEngine?.localStream).length !== 0 &&
+      (deviceIdIsEmpty || deviceList.length === 0)
+    ) {
+      refreshDevices();
+    }
+  }, [rtc]);
 
   return (
     <DeviceContext.Provider
