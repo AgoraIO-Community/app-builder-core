fc37b085e8f7eccc374a83c0b37d08905fd931df
HariharanIT <ghariharanit@gmail.com>
2022-03-31 10:58:44 +0530

    exported max,min view in fpe

--- template/fpe-api/components.ts
+++ template/fpe-api/components.ts
@@ -9,3 +9,6 @@ export * from "../src/assets/icons";
 //Settings
 export * from "../src/components/HostControlView";
 export * from "../src/subComponents/SelectDevice";
+//Video Rendering Component
+export * from "../agora-rn-uikit/src/Views/MaxVideoView";
+export * from "../agora-rn-uikit/src/Views/MinVideoView";
