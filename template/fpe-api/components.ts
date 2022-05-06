ba1aecc5065ecfc154f8b2d815a36e72f86d7d0a
HariharanIT <ghariharanit@gmail.com>
2022-05-06 13:30:34 +0530

    export create and share screen

--- template/fpe-api/components.ts
+++ template/fpe-api/components.ts
@@ -9,3 +9,5 @@ export {default as HostControlView} from '../src/components/HostControlView';
 export {default as SelectDevice} from '../src/subComponents/SelectDevice';
 //Video Rendering Component
 export {default as MaxVideoView} from '../agora-rn-uikit/src/Views/MaxVideoView';
+export {default as Create} from '../src/pages/Create';
+export {default as Share} from '../src/components/Share';
