81671f3c0243ddfa14338d538f5b8ee56bda9d4c
Supriya Adep <supriya@agora.io>
2022-07-20 15:32:28 +0530

    add level 3 implementation for recording

---
+++ template/src/rtm/constants.ts
@@ -0,0 +1,16 @@
+/** RECORDING  **/
+const RECORDING_STARTED = 'RECORDING_STARTED';
+const RECORDING_STOPPED = 'RECORDING_STOPPED';
+const RECORDING_ATTRIBUTE = 'recording';
+/** RECORDING  **/
+
+const EventActions = {
+  RECORDING_STARTED,
+  RECORDING_STOPPED,
+};
+
+const EventNames = {
+  RECORDING_ATTRIBUTE,
+};
+
+export {EventActions, EventNames};
