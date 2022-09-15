6e99a59bd3cc7325b63c67855cd33ca4d0fad551
Supriya Adep <supriya@agora.io>
2022-09-15 15:13:16 +0530

    update types

--- template/fpe-api/fpeEvents.ts
+++ template/fpe-api/fpeEvents.ts
@@ -1,9 +1,9 @@
 // 1. Import Events
 import Events from '../src/rtm-events-api/Events';
-import {EventSourceEnum} from '../src/rtm-events-api';
+import {EventSource} from '../src/rtm-events-api';
 
 // 2. Initialize with source "fpe"
-const fpeEvents = new Events(EventSourceEnum.fpe);
+const fpeEvents = new Events(EventSource.fpe);
 
 // 3. export
 export {fpeEvents as customEvents};
--- template/src/rtm-events-api/types.ts
+++ template/src/rtm-events-api/types.ts
@@ -1,51 +1,22 @@
 import {UidType} from '../../agora-rn-uikit';
-type Simplify<T> = T extends infer S ? {[K in keyof S]: S[K]} : never;
-type NoneOf<T> = Simplify<{[K in keyof T]?: never}>;
-type AtMostOneOf<T> =
-  | NoneOf<T>
-  | {[K in keyof T]: Simplify<Pick<T, K> & NoneOf<Omit<T, K>>>}[keyof T];
 
-// export type TEventOptions = AtMostOneOf<IMessageOptions> & IEventOptions;
+export type ReceiverUidOptions = UidType | UidType[];
 
-export type ToOptions = UidType | UidType[];
+export type EventPayload = string | Record<string, never>;
 
-interface IEventPayloadBase {
-  action?: any;
-}
-
-interface IEventPayloadWithoutAttributes extends IEventPayloadBase {
-  level?: never;
-  value: any;
-}
-
-interface IEventPayloadWithAttributes extends IEventPayloadBase {
-  level: 2 | 3;
-  value: any;
-}
-
-export type EventPayload =
-  | IEventPayloadWithoutAttributes
-  | IEventPayloadWithAttributes
-  | Record<string, never>;
-
-export enum EventSourceEnum {
+export enum EventSource {
   core = 'core',
   fpe = 'fpe',
 }
-export enum EventLevel {
+export enum EventPersistLevel {
   'LEVEL1' = 1,
   'LEVEL2',
   'LEVEL3',
 }
-interface dataPayload {
-  action: string;
-  level: 1 | 2 | 3;
-  value: any;
-}
-interface EvtCbPayload {
-  payload: dataPayload;
-  sender: string;
+interface CbPayload {
+  payload: string;
+  persistLevel: EventPersistLevel;
+  sender: UidType;
   ts: number;
-  source: EventSourceEnum;
 }
-export type TEventCallback = (args: EvtCbPayload) => void;
+export type EventCallbackPayload = (args: CbPayload) => void;
