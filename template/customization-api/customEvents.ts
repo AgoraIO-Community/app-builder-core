40bc66dfaceda4f704c59add72b78451b2c22979
HariharanIT <ghariharanit@gmail.com>
2022-09-22 10:04:22 +0530

    Renamed the custom events

--- template/customization-api/customEvents.ts
+++ template/customization-api/customEvents.ts
@@ -3,7 +3,7 @@ import Events from '../src/rtm-events-api/Events';
 import {EventSource} from '../src/rtm-events-api';
 
 // 2. Initialize with source "fpe"
-const CustomEvents = new Events(EventSource.fpe);
+const customEvents = new Events(EventSource.fpe);
 
 // 3. export
-export {CustomEvents};
+export {customEvents};
