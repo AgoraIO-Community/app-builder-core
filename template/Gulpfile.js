015622c95f228b169435c7fb0512b7784c10ccf6
Supriya Adep <supriya@agora.io>
2022-09-27 18:21:18 +0530

    update electron index path

--- template/Gulpfile.js
+++ template/Gulpfile.js
@@ -81,8 +81,8 @@ const general = {
     // Target specific changes
 
     if (process.env.TARGET === 'rsdk') {
-      if(pkgNameArg == -1){
-        newPackage.name = "@appbuilder/react"
+      if (pkgNameArg == -1) {
+        newPackage.name = '@appbuilder/react';
       }
       newPackage.main = 'index.js';
       newPackage.types = 'index.d.ts';
@@ -98,8 +98,8 @@ const general = {
     }
 
     if (process.env.TARGET === 'wsdk') {
-      if(pkgNameArg == -1){
-        newPackage.name = "@appbuilder/web"
+      if (pkgNameArg == -1) {
+        newPackage.name = '@appbuilder/web';
       }
       newPackage.main = 'app-builder-web-sdk.umd2.js';
       newPackage.types = 'index.d.ts';
@@ -180,7 +180,7 @@ const electron = {
   },
 
   start: (cb) => {
-    runCli('electron .', cb);
+    runCli('electron ./electron/main/index.js', cb);
   },
 };
 
