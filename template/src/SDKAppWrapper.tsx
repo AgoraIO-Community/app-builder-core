4ff7700ca081651f353874a4d60b062b39820347
Aditya Rathore <aditya.26101@gmail.com>
2022-09-30 16:27:38 +0530

    updated join type definition error

--- template/src/SDKAppWrapper.tsx
+++ template/src/SDKAppWrapper.tsx
@@ -1,8 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {
-  CustomizationApiInterface,
-  customize,
-} from 'customization-api';
+import {CustomizationApiInterface, customize} from 'customization-api';
 import {
   customizationConfig,
   CustomizationProvider,
@@ -30,12 +27,10 @@ export interface userEventsMapInterface {
 
 export interface AppBuilderSdkApiInterface {
   customize: (customization: CustomizationApiInterface) => void;
-  createCustomization: (customization: CustomizationApiInterface) => CustomizationApiInterface;
-  join: (
-    roomid: string,
-    resolve: () => void,
-    reject: () => void,
-  ) => Promise<void>;
+  createCustomization: (
+    customization: CustomizationApiInterface,
+  ) => CustomizationApiInterface;
+  join: (roomid: string) => Promise<void>;
   on: <T extends keyof userEventsMapInterface>(
     userEventName: T,
     callBack: userEventsMapInterface[T],
