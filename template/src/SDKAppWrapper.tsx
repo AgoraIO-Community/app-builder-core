4eff6a02884176d52046d1bc9352ee9a13ab2d98
Supriya Adep <supriya@agora.io>
2022-11-03 15:50:18 +0530

    change intialized state

--- template/src/SDKAppWrapper.tsx
+++ template/src/SDKAppWrapper.tsx
@@ -73,6 +73,74 @@ const SDKAppWrapper = () => {
   const [tokenExpiresAt, setTokenExpiresAt] = React.useState(0);
   const [sdkToken, setSdkToken] = React.useState(null);
 
+  const enableSdkAuthentication = (options: OptionsInterface) => {
+    if (options && options.token && options.token.trim() !== '') {
+      // console.log('1. supriya enabling authentication', options.token);
+      setSdkToken(options.token);
+    } else {
+      throw 'Token is missing in the options';
+    }
+  };
+
+  const getNewToken = async () => {
+    await fetch(`${$config.BACKEND_ENDPOINT}/token/refresh`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: sdkToken ? `Bearer ${sdkToken}` : '',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log('7. supriya get new token', data);
+        setSdkToken(data.token || null);
+      });
+  };
+
+  useEffect(() => {
+    const syncSdk = async () => {
+      if (!sdkToken) return;
+      // console.log('2. supriya sdk token set', sdkToken);
+      const decoded = jwt_decode(sdkToken);
+      const expiresAt = decoded?.exp * 1000;
+      if (Date.now() >= expiresAt) {
+        throw 'Token expired. Pass a new token';
+      } else {
+        // console.log('3. supriya emitting token');
+        SDKEvents.emit('sdk-token', sdkToken);
+        setTokenExpiresAt(expiresAt);
+      }
+    };
+    initialized && syncSdk();
+  }, [initialized, sdkToken]);
+
+  useEffect(() => {
+    if (!tokenExpiresAt) return;
+
+    const timer = setInterval(() => {
+      const diffInSeconds = Math.floor(
+        Math.abs(tokenExpiresAt - Date.now()) / 1000,
+      );
+      // console.log('4. supriya diff: ', diffInSeconds);
+      // const minutesLeft = Math.floor(diffInSeconds / 60) % 60;
+      // const secondsLeft = (diffInSeconds - minutes * 60) % 60;
+
+      if (diffInSeconds < REFRESH_TOKEN_DURATION_IN_SEC) {
+        // console.log('5 . supriya 1 min range reached');
+        try {
+          getNewToken();
+          clearInterval(timer);
+        } catch (error) {
+          clearInterval(timer);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [tokenExpiresAt]);
+
   useEffect(() => {
     SDKEvents.on('appInit', async (options, resolve, reject) => {
       try {
@@ -90,72 +158,6 @@ const SDKAppWrapper = () => {
     SDKEvents.emit('addFpeInit');
     // Join event consumed in Create.tsx
   }, []);
-
-  const enableSdkAuthentication = (options: OptionsInterface) => {
-    if (options && options.token && options.token.trim() !== '') {
-      console.log('1. supriya enabling authentication', options.token);
-      setSdkToken(options.token);
-    } else {
-      throw 'Token is missing in the options';
-    }
-  };
-
-  const getNewToken = async () => {
-    await fetch(`${$config.BACKEND_ENDPOINT}/token/refresh`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: sdkToken ? `Bearer ${sdkToken}` : '',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('7. supriya get new token', data);
-        setSdkToken(data.token || null);
-      });
-  };
-
-  useEffect(() => {
-    if (!sdkToken) return;
-    console.log('2. supriya sdk token set', sdkToken);
-    const decoded = jwt_decode(sdkToken);
-    const expiresAt = decoded?.exp * 1000;
-    if (Date.now() >= expiresAt) {
-      throw 'Token expired. Pass a new token';
-    } else {
-      console.log('3. supriya emitting token');
-      SDKEvents.emit('sdk-token', sdkToken);
-      setTokenExpiresAt(expiresAt);
-    }
-  }, [sdkToken]);
-
-  useEffect(() => {
-    if (!tokenExpiresAt) return;
-
-    const timer = setInterval(() => {
-      const diffInSeconds = Math.floor(
-        Math.abs(tokenExpiresAt - Date.now()) / 1000,
-      );
-      console.log('4. supriya diff: ', diffInSeconds);
-      // const minutesLeft = Math.floor(diffInSeconds / 60) % 60;
-      // const secondsLeft = (diffInSeconds - minutes * 60) % 60;
-
-      if (diffInSeconds < REFRESH_TOKEN_DURATION_IN_SEC) {
-        console.log('5 . supriya 1 min range reached');
-        try {
-          getNewToken();
-          clearInterval(timer);
-        } catch (error) {
-          clearInterval(timer);
-        }
-      }
-    }, 1000);
-
-    return () => {
-      console.log('6. supriya timer unmounted for', tokenExpiresAt);
-      clearInterval(timer);
-    };
-  }, [tokenExpiresAt]);
 
   return (
     <>
