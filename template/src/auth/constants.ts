6a2a8c5fe12e28f724d6140a524a5d557dda1152
Supriya Adep <supriya@agora.io>
2022-11-23 17:22:54 +0530

    fix constants

--- template/src/auth/constants.ts
+++ template/src/auth/constants.ts
@@ -1,6 +1,7 @@
 const BACKEND_URL = $config.BACKEND_ENDPOINT;
-const VERCEL_URL = process.env.AGORA_APP_VERCEL_URL;
+const VERCEL_URL =
+  'https://app-builder-core-git-api-managed-service-backend-agoraio.vercel.app';
 const AUTH_ENDPOINT_URL = `${$config.BACKEND_ENDPOINT}/idp/login`;
-const AUTH_REDIRECT_URL = `${process.env.AGORA_APP_VERCEL_URL}/authorize`;
+const AUTH_REDIRECT_URL = `${VERCEL_URL}/authorize`;
 
 export {BACKEND_URL, VERCEL_URL, AUTH_ENDPOINT_URL, AUTH_REDIRECT_URL};
