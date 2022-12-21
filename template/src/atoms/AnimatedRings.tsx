d76944393018c6b9f5128a2f74796b84c89cbd5a
Bhupendra Singh Negi <bhupendra1011@gmail.com>
2022-12-21 10:54:19 +0530

    Feature/animation (#268)
    * adding animation for web active sp

--- template/src/atoms/AnimatedRings.tsx
+++ template/src/atoms/AnimatedRings.tsx
@@ -2,23 +2,56 @@ import {StyleSheet, Text, View} from 'react-native';
 import hexadecimalTransparency from '../utils/hexadecimalTransparency';
 import React from 'react';
 
+const Ring = ({isMobileView}) => {
+  const css = `.ring {
+    width: ${isMobileView ? '60px' : '100px'};
+    height: ${isMobileView ? '60px' : '100px'};
+    border-radius: 50%;
+    position: absolute;
+    background-color: transparent;
+    border: 3px ${$config.PRIMARY_ACTION_BRAND_COLOR} solid;
+    -webkit-animation-name: ani; 
+    -webkit-animation-iteration-count: infinite;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation-duration: 3s;
+    -webkit-animation-direction: normal;
+   
+}
+
+@-webkit-keyframes ani {
+    0% {-webkit-transform: scale(0.5); opacity: 0} 
+    1% {-webkit-transform: scale(0.5); opacity: 1} 
+    95% {-webkit-transform: scale(1.5); opacity: .5;}
+    100% {-webkit-transform: scale(1.5); opacity: 0;}
+}
+#r2 { -webkit-animation-delay: -1s;}
+#r3 { -webkit-animation-delay: -2s;}
+`;
+  return (
+    <div>
+      <style type="text/css">{css}</style>
+      <div id="r1" className="ring"></div>
+      <div id="r2" className="ring"></div>
+      <div id="r3" className="ring"></div>
+    </div>
+  );
+};
+
 const AnimatedRings = ({isActiveSpeaker, children, isMobileView}) => {
   return (
     <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: isActiveSpeaker
-            ? $config.PRIMARY_ACTION_BRAND_COLOR +
-              hexadecimalTransparency['15%']
-            : 'transparent',
-        },
-        {
-          width: isMobileView ? 100 : 140,
-          height: isMobileView ? 100 : 140,
-          borderRadius: isMobileView ? 50 : 80,
-        },
-      ]}>
+      style={{
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+      }}>
+      {isActiveSpeaker ? (
+        <View style={{position: 'relative', left: -2, top: -2}}>
+          <Ring isMobileView={isMobileView} />
+        </View>
+      ) : (
+        <></>
+      )}
       {children}
     </View>
   );
