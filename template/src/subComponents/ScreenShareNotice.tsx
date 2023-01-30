b0ff9428454b721d335f5b4b85334efd3485843c
HariharanIT <ghariharanit@gmail.com>
2023-01-30 20:59:04 +0530

    Fixed screensharing notice border radius

--- template/src/subComponents/ScreenShareNotice.tsx
+++ template/src/subComponents/ScreenShareNotice.tsx
@@ -111,7 +111,7 @@ const styles = StyleSheet.create({
     zIndex: 2,
     backgroundColor:
       $config.HARD_CODED_BLACK_COLOR + hexadecimalTransparency['80%'],
-    borderRadius: 8,
+    borderRadius: 4,
   },
   screensharingMessageMin: {
     alignSelf: 'center',
