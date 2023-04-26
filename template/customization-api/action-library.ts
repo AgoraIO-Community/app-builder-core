04cfc99bbdb6b26f2ed7b82c4e1766321e191d99
HariharanIT <ghariharanit@gmail.com>
2023-04-26 16:56:38 +0530

    Added useSwitchCamera in action lib

--- template/customization-api/action-library.ts
+++ template/customization-api/action-library.ts
@@ -4,6 +4,8 @@ export {default as useJoinRoom} from '../src/utils/useJoinRoom';
 
 //remove remote user from the call
 export {default as useRemoteEndcall} from '../src/utils/useRemoteEndCall';
+//switch camera
+export {default as useSwitchCamera} from '../src/utils/useSwitchCamera';
 
 export {controlMessageEnum} from '../src/components/ChatContext';
 //audio/video toggle state
