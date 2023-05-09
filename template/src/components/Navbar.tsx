927ac83271b4c20016e7640890429b95d61776a0
HariharanIT <ghariharanit@gmail.com>
2023-05-09 19:10:28 +0530

    Fixed recording status showing by default

--- template/src/components/Navbar.tsx
+++ template/src/components/Navbar.tsx
@@ -346,7 +346,7 @@ const Navbar = () => {
         <ToolbarItem>
           <ParticipantsCount />
         </ToolbarItem>
-        {!isRecordingActive ? (
+        {isRecordingActive ? (
           <ToolbarItem>
             <RecordingInfo
               recordingLabel={isHorizontal ? recordingLabel : ''}
