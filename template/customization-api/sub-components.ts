8d157c2cbc80f0683b29d6d675150008cf8a2d60
HariharanIT <ghariharanit@gmail.com>
2023-05-10 18:57:21 +0530

    Added toolbar export

--- template/customization-api/sub-components.ts
+++ template/customization-api/sub-components.ts
@@ -50,6 +50,7 @@ export {
   ClientRole as UikitClientRole,
   ChannelProfile as UikitChannelProfile,
 } from '../agora-rn-uikit';
+export {default as Toolbar} from '../src/atoms/Toolbar';
 export {default as ToolbarItem} from '../src/atoms/ToolbarItem';
 export {default as ToolbarPreset} from '../src/atoms/ToolbarPreset';
 export type {
