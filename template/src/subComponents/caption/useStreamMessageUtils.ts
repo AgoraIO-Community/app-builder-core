3f664352edbb4f4050771a702c599c548f13ac5e
Bhupendra Negi <bhupendra1011@gmail.com>
2023-07-13 13:17:45 +0530

    refactored streamMessageCallback util to hook and updated logic of updating transcript

--- template/src/subComponents/caption/useStreamMessageUtils.ts
+++ template/src/subComponents/caption/useStreamMessageUtils.ts
@@ -8,121 +8,156 @@ type StreamMessageCallback = (args: [number, Uint8Array]) => void;
 type FinalListType = {
   [key: string]: string[];
 };
-
+type TranscriptItem = {
+  name: string;
+  uid: string;
+  time: number;
+  text: string;
+};
 const useStreamMessageUtils = (): {
   streamMessageCallback: StreamMessageCallback;
-
   downloadTranscript: () => void;
 } => {
-  const {captionObj, meetingTranscript, setCaptionObj, setMeetingTranscript} =
-    useCaption();
-  const meetingTranscriptRef = useRef(meetingTranscript);
+  const {meetingTranscript, setCaptionObj, setMeetingTranscript} = useCaption();
   const startTimeRef = useRef<number>(0);
   const finalList = useRef<FinalListType>({});
   const {renderList} = useRender();
   const renderListRef = useRef({renderList});
 
+  /* renderlist was not updating when new user joins the call */
   useEffect(() => {
     renderListRef.current.renderList = renderList;
   }, [renderList]);
 
   //  handles the stream messages and updates the state variables.
   const streamMessageCallback: StreamMessageCallback = (args) => {
-    const [uid, payload] = args; // uid is of the bot which sends the stream messages in the channel
+    /* uid - bot which sends stream message in channel
+       payload - stream message in Uint8Array format
+      */
+    const [uid, payload] = args;
     let nonFinalList = []; // holds intermediate results
     let currentText = ''; // holds current caption
+    let currentTranscript: TranscriptItem = {
+      name: '',
+      uid: '',
+      time: 0,
+      text: '',
+    };
+
     const textstream = protoRoot
       .lookupType('Text')
       .decode(payload as Uint8Array) as any;
     console.log('STT - Parsed Textstream : ', textstream);
 
+    // identifying speaker of caption
     const userName =
-      renderListRef.current.renderList[textstream.uid]?.name || 'Speaker'; // identifying speaker of caption
-
-    // creating [] for each user to store thier complete transcripts
-
+      renderListRef.current.renderList[textstream.uid]?.name || 'Speaker';
+    /* creating [] for each user to store their complete transcripts
+       ex: {282190954:[]}
+    */
     if (!finalList.current[textstream.uid]) {
       finalList.current[textstream.uid] = [];
     }
 
-    const words = textstream.words;
+    const words = textstream.words; //[Word,Word]
 
-    // categorize words into final & nonFinal objects per uid
-    //{112:['hi','2','4'],113:['hello']}
-    words.map((word) => {
+    /* categorize words into final & nonFinal objects per uid
+      Final Word Ex : {
+      "text": "Hello, are you doing?",
+      "durationMs": 960,
+      "isFinal": true,
+      "confidence": 0.8549408316612244
+      }
+  */
+    for (const word of words) {
       if (word.isFinal) {
         finalList.current[textstream.uid].push(word.text);
-
         currentText += word.text;
-
+        // log info to show measure the duration of passes in which a sentence gets finalized
         const duration = performance.now() - startTimeRef.current;
         console.log(
           `stt-Time taken to finalize caption ${currentText}: ${duration}ms`,
         );
         startTimeRef.current = null; // Reset start time
       } else {
-        nonFinalList.push(word.text);
+        word.text !== '.' && nonFinalList.push(word.text);
         if (!startTimeRef.current) {
           startTimeRef.current = performance.now();
         }
       }
-    });
+    }
 
+    /* after getting the final word in the streamMessage 
+       checking if previous transcript be updated or new entry should be added   
+     */
     if (currentText.length) {
-      let flag = false;
-      // check for last itm only
-      meetingTranscriptRef.current.forEach((item) => {
-        if (
-          item.uid == textstream.uid &&
-          new Date().getTime() - item.time < 30000
-        ) {
-          item.text = item.text + ' ' + currentText;
-          flag = true;
-          // update existing transcript for uid & time
-        }
-      });
+      let shouldTranscriptBeUpdated = false;
 
-      if (!flag) {
-        // update with prev history
-        meetingTranscriptRef.current.push({
-          name: userName,
-          uid: textstream.uid, //1
-          time: new Date().getTime(), // t1  //textstream.time, // textstream.time returing value 699391063 - which is not comparable with timestamp
-          text: currentText,
-        });
-      }
-      // 1 4.01  1 4.02  1.4.06
-
-      // update the last update only
-      // chnage setMeetTranscipt to ref setMeetingTranscriptRef.current = meetingTranscriptRef.current
       setMeetingTranscript((prevTranscript) => {
-        return [...meetingTranscriptRef.current];
+        // getting the last item in the transcript
+        currentTranscript =
+          prevTranscript.length > 0
+            ? prevTranscript[prevTranscript.length - 1]
+            : null;
+        /* 
+          checking if the last item transcript matches with current uid and there is delay of less than 3 sec.
+          If yes then updating the last transcript msg with current text
+          If no then adding a new entry in the transcript  
+        */
+        if (
+          currentTranscript &&
+          currentTranscript.uid === textstream.uid &&
+          new Date().getTime() - currentTranscript.time < 30000
+        ) {
+          currentTranscript.text = currentTranscript.text + ' ' + currentText;
+          shouldTranscriptBeUpdated = true;
+        }
+
+        if (shouldTranscriptBeUpdated && currentTranscript) {
+          // updating existing
+          prevTranscript[prevTranscript.length - 1] = currentTranscript;
+          return prevTranscript;
+        } else {
+          // adding new entry
+          return [
+            ...prevTranscript,
+            {
+              name: userName,
+              uid: textstream.uid,
+              time: new Date().getTime(),
+              text: currentText,
+            },
+          ];
+        }
       });
     }
 
-    // including prev references of the caption
+    /* 
+     stringBuilder- used to create strings for live captioning
+     Previous final words of the uid are prepended and 
+     then current non final words so that context of speech is not lost
+    */
     let stringBuilder = finalList?.current[textstream.uid]?.join(' ');
     stringBuilder += stringBuilder?.length > 0 ? ' ' : '';
     stringBuilder += nonFinalList?.join(' ');
 
-    // when stringBuilder is '' then it will clear the live captions when person stops speaking or mic muted
+    // If person is not speaking or mic is muted, then we don't want to show live captions
     if (textstream.words.length === 0) {
-      stringBuilder = '';
+      //stringBuilder = '';
     }
 
     setCaptionObj((prevState) => ({
       ...prevState,
       [textstream.uid]: stringBuilder,
-    }));
+    })); // live captions will appear with nonfinal words
 
     if (textstream.words.length === 0) {
-      // clearing prev sel when there is pause so that live captions show current spoken words
+      // clearing prev live captions for a uid when there is pause so that live captions show only current spoken words
       finalList.current[textstream.uid] = [];
     }
 
     console.group('STT-logs');
     console.log('stt-finalList =>', finalList.current);
-    console.log('stt - meeting transcript =>', meetingTranscriptRef.current);
     console.log('stt - current text =>', currentText);
     console.groupEnd();
   };
