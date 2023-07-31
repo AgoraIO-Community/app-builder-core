db5bbf3f555911d81473e869024ec72b3eeaa1c4
HariharanIT <ghariharanit@gmail.com>
2023-07-31 10:39:34 +0530

    Fixed issue on pinch zoom not working on ios

--- template/src/pages/video-call/ZoomableWrapper.native.tsx
+++ template/src/pages/video-call/ZoomableWrapper.native.tsx
@@ -1,27 +1,59 @@
 import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
+import {isAndroid} from '../../utils/common';
 import React from 'react';
-import {View} from 'react-native';
+import {Animated, View} from 'react-native';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import {useSharedValue} from 'react-native-reanimated';
+
 const ZoomableWrapper = (props) => {
   if (!props?.enableZoom) {
     return <>{props.children}</>;
   }
-  return (
-    <View
-      style={{
-        flexShrink: 1,
-        height: '100%',
-        width: '100%',
-      }}>
-      <ReactNativeZoomableView
-        maxZoom={30}
-        zoomStep={1}
-        initialZoom={1}
+  if (isAndroid()) {
+    return (
+      <View
         style={{
-          flex: 1,
+          flexShrink: 1,
+          height: '100%',
+          width: '100%',
         }}>
+        <ReactNativeZoomableView
+          maxZoom={30}
+          zoomStep={1}
+          initialZoom={1}
+          style={{
+            flex: 1,
+          }}>
+          {props.children}
+        </ReactNativeZoomableView>
+      </View>
+    );
+  }
+
+  const scale = useSharedValue(1);
+  const savedScale = useSharedValue(1);
+
+  const pinchGesture = Gesture.Pinch()
+    .onUpdate((e) => {
+      scale.value = savedScale.value * e.scale;
+    })
+    .onEnd(() => {
+      savedScale.value = scale.value;
+    });
+
+  return (
+    <GestureDetector gesture={pinchGesture}>
+      <Animated.View
+        style={[
+          {
+            width: '100%',
+            height: '100%',
+          },
+          {transform: [{scale: scale.value}]},
+        ]}>
         {props.children}
-      </ReactNativeZoomableView>
-    </View>
+      </Animated.View>
+    </GestureDetector>
   );
 };
 export default ZoomableWrapper;
