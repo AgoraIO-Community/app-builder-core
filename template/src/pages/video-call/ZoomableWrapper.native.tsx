7ef25bfb2d08d4a70b4fd0a38f7d09226bed163c
HariharanIT <ghariharanit@gmail.com>
2023-07-31 12:48:10 +0530

    Added animation while zoom in/out on ios screenshare view

--- template/src/pages/video-call/ZoomableWrapper.native.tsx
+++ template/src/pages/video-call/ZoomableWrapper.native.tsx
@@ -1,6 +1,6 @@
 import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
 import {isAndroid} from '../../utils/common';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {Animated, View} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import {useSharedValue} from 'react-native-reanimated';
@@ -29,16 +29,45 @@ const ZoomableWrapper = (props) => {
       </View>
     );
   }
+  /**
+   * IOS - SurfaceView having some issue ReactNativeZoomableView lib for zoom
+   * SurfaceView blocking pinch gesture from ReactNativeZoomableView lib
+   * so below added pinch gesture to enable zoom in/out option on ios
+   */
 
-  const scale = useSharedValue(1);
-  const savedScale = useSharedValue(1);
+  const startValue = useRef(new Animated.Value(1));
+  const lastSavedValue = useRef(1);
+  const updatedValue = useRef(1);
+  const duration = 250;
+
+  function debounce(func, timeout = 200) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
+
+  useEffect(() => {
+    Animated.timing(startValue.current, {
+      toValue: updatedValue.current,
+      duration: duration,
+      useNativeDriver: true,
+    }).start();
+  }, [updatedValue.current]);
 
   const pinchGesture = Gesture.Pinch()
-    .onUpdate((e) => {
-      scale.value = savedScale.value * e.scale;
-    })
+    .runOnJS(true)
+    .onUpdate(
+      debounce((e) => {
+        updatedValue.current = lastSavedValue.current * e.scale;
+      }),
+    )
     .onEnd(() => {
-      savedScale.value = scale.value;
+      lastSavedValue.current = updatedValue.current;
+      startValue.current = new Animated.Value(updatedValue.current);
     });
 
   return (
@@ -49,7 +78,7 @@ const ZoomableWrapper = (props) => {
             width: '100%',
             height: '100%',
           },
-          {transform: [{scale: scale.value}]},
+          {transform: [{scale: startValue.current}]},
         ]}>
         {props.children}
       </Animated.View>
