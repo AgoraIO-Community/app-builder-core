ea64f2cad2d58b5b4c0f8ae12598e4de77444d87
HariharanIT <ghariharanit@gmail.com>
2023-08-01 17:04:41 +0530

    Fixed ios pan and pinch zoom on screenshare view

--- template/src/pages/video-call/ZoomableWrapper.native.tsx
+++ template/src/pages/video-call/ZoomableWrapper.native.tsx
@@ -1,17 +1,14 @@
-// import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
-// import {isAndroid} from '../../utils/common';
-import React, {useEffect, useRef} from 'react';
-// import {Animated, View} from 'react-native';
-// import {Gesture, GestureDetector} from 'react-native-gesture-handler';
-// import {useSharedValue} from 'react-native-reanimated';
+import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
+import {isAndroid} from '../../utils/common';
+import React from 'react';
+import {View} from 'react-native';
 import {PinchableView} from './PinchableView';
 
 const ZoomableWrapper = (props) => {
   if (!props?.enableZoom) {
     return <>{props.children}</>;
   }
-  return <PinchableView>{props.children}</PinchableView>;
-  /**
+
   if (isAndroid()) {
     return (
       <View
@@ -32,53 +29,6 @@ const ZoomableWrapper = (props) => {
       </View>
     );
   }
-
-  const startValue = useRef(new Animated.Value(1));
-  const lastSavedValue = useRef(1);
-  const updatedValue = useRef(1);
-  const duration = 250;
-
-  function debounce(func, timeout = 100) {
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        func.apply(this, args);
-      }, timeout);
-    };
-  }
-
-  useEffect(() => {
-    Animated.spring(startValue.current, {
-      toValue: updatedValue.current,
-      delay: 250,
-      useNativeDriver: true,
-    }).start();
-  }, [updatedValue.current]);
-
-  const pinchGesture = Gesture.Pinch()
-    .onUpdate((e) => {
-      updatedValue.current = lastSavedValue.current * e.scale;
-    })
-    .onEnd(() => {
-      lastSavedValue.current = updatedValue.current;
-      startValue.current = new Animated.Value(updatedValue.current);
-    });
-
-  return (
-    <GestureDetector gesture={pinchGesture}>
-      <Animated.View
-        style={[
-          {
-            width: '100%',
-            height: '100%',
-          },
-          {transform: [{scale: startValue.current}]},
-        ]}>
-        {props.children}
-      </Animated.View>
-    </GestureDetector>
-  );
-   */
+  return <PinchableView>{props.children}</PinchableView>;
 };
 export default ZoomableWrapper;
