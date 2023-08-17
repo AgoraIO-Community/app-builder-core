b1422f88b38c21fc4d0ebd51291dcd84adf303e5
Bhupendra Negi <bhupendra1011@gmail.com>
2023-08-17 18:56:04 +0530

    without PQ Lib

--- template/src/subComponents/caption/useStreamMessageUtils.ts
+++ template/src/subComponents/caption/useStreamMessageUtils.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useCaption} from './useCaption';
 import protoRoot from './proto/ptoto';
-import PQueue from 'p-queue';
 
 type StreamMessageCallback = (args: [number, Uint8Array]) => void;
 type FinalListType = {
@@ -26,26 +25,24 @@ const useStreamMessageUtils = (): {
   let captionStartTime: number = 0;
   const finalList: FinalListType = {};
   const finalTranscriptList: FinalListType = {};
-  const queue = new PQueue({concurrency: 1});
 
   const streamMessageCallback: StreamMessageCallback = (args) => {
-    const queueCallback = (args: [number, Uint8Array]) => {
-      /* uid - bot which sends stream message in channel
+    /* uid - bot which sends stream message in channel
        payload - stream message in Uint8Array format
       */
-      const [uid, payload] = args;
-      let nonFinalText = ''; // holds intermediate results
-      let currentFinalText = ''; // holds current caption
-      let isInterjecting = false;
+    const [uid, payload] = args;
+    let nonFinalText = ''; // holds intermediate results
+    let currentFinalText = ''; // holds current caption
+    let isInterjecting = false;
 
-      const textstream = protoRoot
-        .lookupType('Text')
-        .decode(payload as Uint8Array) as any;
+    const textstream = protoRoot
+      .lookupType('Text')
+      .decode(payload as Uint8Array) as any;
 
-      console.log('STT - Parsed Textstream : ', textstream);
+    console.log('STT - Parsed Textstream : ', textstream);
 
-      // Identifing Current & Prev Speakers for the Captions
-      /*
+    // Identifing Current & Prev Speakers for the Captions
+    /*
       t0 A : Hi All!
       t1 B : Hi A
       t2 A : Hi B
@@ -73,28 +70,28 @@ const useStreamMessageUtils = (): {
 
     */
 
-      if (textstream.uid !== activeSpeakerRef.current) {
-        // we have a speaker change so clear the context for prev speaker
-        if (prevSpeakerRef.current !== '') {
-          finalList[prevSpeakerRef.current] = [];
-          isInterjecting = true;
-        }
-        prevSpeakerRef.current = activeSpeakerRef.current;
-        activeSpeakerRef.current = textstream.uid;
+    if (textstream.uid !== activeSpeakerRef.current) {
+      // we have a speaker change so clear the context for prev speaker
+      if (prevSpeakerRef.current !== '') {
+        finalList[prevSpeakerRef.current] = [];
+        isInterjecting = true;
       }
+      prevSpeakerRef.current = activeSpeakerRef.current;
+      activeSpeakerRef.current = textstream.uid;
+    }
 
-      /* creating [] for each user to store their complete transcripts
+    /* creating [] for each user to store their complete transcripts
        initializing captions state for cuurent speaker
        ex: {282190954:[]}
     */
-      if (!finalList[textstream.uid]) {
-        finalList[textstream.uid] = [];
-        finalTranscriptList[textstream.uid] = [];
-      }
+    if (!finalList[textstream.uid]) {
+      finalList[textstream.uid] = [];
+      finalTranscriptList[textstream.uid] = [];
+    }
 
-      const words = textstream.words; //[Word,Word]
+    const words = textstream.words; //[Word,Word]
 
-      /* categorize words into final & nonFinal objects per uid
+    /* categorize words into final & nonFinal objects per uid
       Final Word Ex : {
       "text": "Hello, are you doing?",
       "durationMs": 960,
@@ -102,104 +99,98 @@ const useStreamMessageUtils = (): {
       "confidence": 0.8549408316612244
       }
   */
-      for (const word of words) {
-        if (word.isFinal) {
-          finalList[textstream.uid].push(word.text);
-          finalTranscriptList[textstream.uid].push(word.text);
-          currentFinalText = word.text;
-          // log info to show measure the duration of passes in which a sentence gets finalized
-          const duration = performance.now() - captionStartTime;
-          console.log(
-            `stt-Time taken to finalize caption ${currentFinalText}: ${duration}ms`,
-          );
-          captionStartTime = null; // Reset start time
-        } else {
-          nonFinalText = word.text !== '.' ? word.text : nonFinalText;
-          if (!captionStartTime) {
-            captionStartTime = performance.now();
-          }
+    for (const word of words) {
+      if (word.isFinal) {
+        finalList[textstream.uid].push(word.text);
+        finalTranscriptList[textstream.uid].push(word.text);
+        currentFinalText = word.text;
+        // log info to show measure the duration of passes in which a sentence gets finalized
+        const duration = performance.now() - captionStartTime;
+        console.log(
+          `stt-Time taken to finalize caption ${currentFinalText}: ${duration}ms`,
+        );
+        captionStartTime = null; // Reset start time
+      } else {
+        nonFinalText = word.text !== '.' ? word.text : nonFinalText;
+        if (!captionStartTime) {
+          captionStartTime = performance.now();
         }
       }
+    }
 
-      /* Updating Meeting Transcript */
-      if (currentFinalText.length) {
-        setMeetingTranscript((prevTranscript) => {
-          const lastTranscriptIndex = prevTranscript.length - 1;
-          const lastTranscript =
-            lastTranscriptIndex >= 0
-              ? prevTranscript[lastTranscriptIndex]
-              : null;
+    /* Updating Meeting Transcript */
+    if (currentFinalText.length) {
+      setMeetingTranscript((prevTranscript) => {
+        const lastTranscriptIndex = prevTranscript.length - 1;
+        const lastTranscript =
+          lastTranscriptIndex >= 0 ? prevTranscript[lastTranscriptIndex] : null;
 
-          /*
+        /*
             checking if the last item transcript matches with current uid 
             If yes then updating the last transcript msg with current text
             If no then adding a new entry in the transcript
           */
-          if (lastTranscript && lastTranscript.uid === textstream.uid) {
-            const updatedTranscript = {
-              ...lastTranscript,
-              //text: lastTranscript.text + ' ' + currentFinalText, // missing few updates with reading prev values
-              text: finalTranscriptList[textstream.uid].join(' '),
-            };
+        if (lastTranscript && lastTranscript.uid === textstream.uid) {
+          const updatedTranscript = {
+            ...lastTranscript,
+            //text: lastTranscript.text + ' ' + currentFinalText, // missing few updates with reading prev values
+            text: finalTranscriptList[textstream.uid].join(' '),
+          };
 
-            return [
-              ...prevTranscript.slice(0, lastTranscriptIndex),
-              updatedTranscript,
-            ];
-          } else {
-            const isLangUpdate =
-              lastTranscript?.uid.toString().indexOf('langUpdate') > -1;
+          return [
+            ...prevTranscript.slice(0, lastTranscriptIndex),
+            updatedTranscript,
+          ];
+        } else {
+          const isLangUpdate =
+            lastTranscript?.uid.toString().indexOf('langUpdate') > -1;
 
-            finalTranscriptList[textstream.uid] = [currentFinalText];
+          finalTranscriptList[textstream.uid] = [currentFinalText];
 
-            return [
-              ...prevTranscript,
-              {
-                uid: textstream.uid,
-                time: new Date().getTime(),
-                text: currentFinalText,
-              },
-            ];
-          }
-        });
-      }
+          return [
+            ...prevTranscript,
+            {
+              uid: textstream.uid,
+              time: new Date().getTime(),
+              text: currentFinalText,
+            },
+          ];
+        }
+      });
+    }
 
-      /* 
+    /* 
      Previous final words of the uid are prepended and 
      then current non final words so that context of speech is not lost
     */
-      const existingStringBuffer = isInterjecting
-        ? ''
-        : finalList[textstream.uid]?.join(' ');
-      const latestString = nonFinalText;
-      const captionText =
-        existingStringBuffer.length > 0
-          ? existingStringBuffer + ' ' + latestString
-          : latestString;
+    const existingStringBuffer = isInterjecting
+      ? ''
+      : finalList[textstream.uid]?.join(' ');
+    const latestString = nonFinalText;
+    const captionText =
+      existingStringBuffer.length > 0
+        ? existingStringBuffer + ' ' + latestString
+        : latestString;
 
-      // updating the captions
-      captionText &&
-        setCaptionObj((prevState) => {
-          return {
-            ...prevState,
-            [textstream.uid]: {
-              text: captionText,
-              lastUpdated: new Date().getTime(),
-            },
-          };
-        });
+    // updating the captions
+    captionText &&
+      setCaptionObj((prevState) => {
+        return {
+          ...prevState,
+          [textstream.uid]: {
+            text: captionText,
+            lastUpdated: new Date().getTime(),
+          },
+        };
+      });
 
-      console.group('STT-logs');
-      console.log('Recived uid =>', textstream.uid);
-      console.log('PrevSpeaker uid =>', prevSpeakerRef.current);
-      console.log('ActiveSpeaker uid=>', activeSpeakerRef.current);
-      console.log('final List =>', finalList);
-      console.groupEnd();
-    };
-    (async () => {
-      await queue.add(() => queueCallback(args));
-      console.log('stt- using pq queue');
-    })();
+    console.group('STT-logs');
+    console.log('--without PQ--');
+    console.log('Recived uid =>', textstream.uid);
+    console.log('PrevSpeaker uid =>', prevSpeakerRef.current);
+    console.log('ActiveSpeaker uid=>', activeSpeakerRef.current);
+    console.log('final List =>', finalList);
+    console.groupEnd();
   };
 
   return {
