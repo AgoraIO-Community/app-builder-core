d2fa5c0ad43b8502c31bc07bf73074d9214a6c81
HariharanIT <ghariharanit@gmail.com>
2023-08-23 18:58:59 +0530

    Added fix for bottomsheet closing on android

--- template/src/pages/video-call/ActionSheet.native.tsx
+++ template/src/pages/video-call/ActionSheet.native.tsx
@@ -70,13 +70,14 @@ const ActionSheet = () => {
         break;
       }
       case SidePanelType.None: {
-        if (isAndroid) {
+        if (isAndroid()) {
           timeout = setTimeout(() => {
             // Code to be executed after the timeout until https://github.com/gorhom/react-native-bottom-sheet/pull/1164/files is merged
             chatSheetRef?.current.close();
             participantsSheetRef?.current.close();
             settingsSheetRef?.current.close();
             transcriptSheetRef?.current.close();
+            bottomSheetRef?.current.present();
           }, 200);
         } else {
           // Code to be executed immediately without a timer
