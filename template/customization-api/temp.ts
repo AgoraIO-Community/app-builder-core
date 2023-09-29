d64eee447889bce0a5eb4a653562ad5e5e147f43
HariharanIT <ghariharanit@gmail.com>
2023-09-29 17:11:05 +0530

    Exported whiteboard context and view

--- template/customization-api/temp.ts
+++ template/customization-api/temp.ts
@@ -3,4 +3,13 @@
 import VideoRenderer from '../src/pages/video-call/VideoRenderer';
 import {DispatchContext} from '../agora-rn-uikit';
 import IconButton from '../src/atoms/IconButton';
-export {VideoRenderer, DispatchContext, IconButton};
+import WhiteboardView from '../src/components/whiteboard/WhiteboardView';
+import {whiteboardContext} from '../src/components/whiteboard/WhiteboardConfigure';
+
+export {
+  VideoRenderer,
+  DispatchContext,
+  IconButton,
+  WhiteboardView,
+  whiteboardContext,
+};
