5625596c337615be596d9dedaf68f267853082cb
Bhupendra Negi <bhupendra1011@gmail.com>
2023-11-24 12:24:39 +0530

    feeback 2-precall video preview

--- template/src/components/virtual-background/VBPanel.tsx
+++ template/src/components/virtual-background/VBPanel.tsx
@@ -355,6 +355,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 12,
+    borderWidth: 1,
+    borderColor: $config.INPUT_FIELD_BORDER_COLOR,
   },
   active: {
     borderWidth: 2,
@@ -364,6 +366,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
+    borderRadius: 4,
   },
   triangle: {
     width: 0,
