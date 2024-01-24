4fff4a3f970b9446ab088d1280184cc70ad63001
Bhupendra Negi <bhupendra1011@gmail.com>
2024-01-24 19:42:30 +0530

    fallback for native chat emoji

---
+++ template/src/subComponents/chat/ChatEmojiButton.native.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ChatEmojiButton = () => {
+  return null;
+};
+
+export default ChatEmojiButton;
+
+const styles = StyleSheet.create({});
