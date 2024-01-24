8f29d7e4a68de7d6b4b893fa71a3e052a03a68de
HariharanIT <ghariharanit@gmail.com>
2024-01-24 19:08:29 +0530

    Added fix to handle missing magic variable

--- template/configTransform.js
+++ template/configTransform.js
@@ -15,4 +15,19 @@ let config = {};
 Object.keys(jsonFile).map((key) => {
   config[`${PREFIX}.${key}`] = jsonFile[key];
 });
+
+//find any missing config
+const defaultConfig = require('./defaultConfig');
+const filteredArray = Object.keys(defaultConfig).filter(
+  (value) => !Object.keys(jsonFile).includes(value),
+);
+
+//add missing config with default value
+if (filteredArray && filteredArray?.length) {
+  console.error('config.json missing ', filteredArray);
+  filteredArray.forEach((i) => {
+    config[`${PREFIX}.${i}`] = defaultConfig[i];
+  });
+}
+
 module.exports = config;
