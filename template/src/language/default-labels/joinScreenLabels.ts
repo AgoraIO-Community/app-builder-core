67237a6d438f818526ef10dbed5bc3387690d721
HariharanIT <ghariharanit@gmail.com>
2024-01-24 13:28:59 +0530

    Added i18n for join screen

--- template/src/language/default-labels/joinScreenLabels.ts
+++ template/src/language/default-labels/joinScreenLabels.ts
@@ -1,13 +1,25 @@
 import {I18nBaseType} from '../i18nTypes';
 
 export interface I18nJoinScreenLabelsInterface {
-  //commented for v1 release
-  // meetingIdInputPlaceholder?: I18nBaseType; //
-  // enterMeetingButton?: I18nBaseType; //
+  roomId?: I18nBaseType;
+  streamId?: I18nBaseType;
+  enterRoomId?: I18nBaseType;
+  enterStreamId?: I18nBaseType;
+  joinRoom?: I18nBaseType;
+  joinStream?: I18nBaseType;
+  createStream?: I18nBaseType;
+  invalidRoomIdToastHeading?: I18nBaseType;
+  invalidRoomIdToastSubheading?: I18nBaseType;
 }
 
 export const JoinScreenLabels: I18nJoinScreenLabelsInterface = {
-  //commented for v1 release
-  // meetingIdInputPlaceholder: 'Enter Room ID',
-  // enterMeetingButton: 'Enter Room',
+  roomId: 'Room ID',
+  streamId: 'Stream ID',
+  enterRoomId: 'Enter Room ID',
+  enterStreamId: 'Enter Stream ID',
+  joinRoom: 'Join Room',
+  joinStream: 'Join Stream',
+  createStream: 'Create a Stream',
+  invalidRoomIdToastHeading: 'Room ID Invalid.',
+  invalidRoomIdToastSubheading: 'Please enter a valid Room ID',
 };
