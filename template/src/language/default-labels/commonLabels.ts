0e720901aee887863803d9ff4a6ffded5d03384f
HariharanIT <ghariharanit@gmail.com>
2024-02-01 16:06:32 +0530

    Updated the create screen translation

--- template/src/language/default-labels/commonLabels.ts
+++ template/src/language/default-labels/commonLabels.ts
@@ -1,24 +1,11 @@
 import {I18nBaseType} from '../i18nTypes';
 
 export interface I18nCommonLabelsInterface {
-  //commented for v1 release
-  // goBackButton?: I18nBaseType; //
-  // logoutButton?: I18nBaseType; //
-  // googleAuthButton?: I18nBaseType; //
-  // microsoftAuthButton?: I18nBaseType; //
-  // slackAuthButton?: I18nBaseType; //
-  // appleAuthButton?: I18nBaseType; //
   cancel?: I18nBaseType;
+  loadingText?: I18nBaseType;
 }
 
 export const CommonLabels: I18nCommonLabelsInterface = {
   cancel: 'CANCEL',
-
-  //commented for v1 release
-  // goBackButton: 'Go back',
-  // logoutButton: 'Logout',
-  // googleAuthButton: 'Google',
-  // microsoftAuthButton: 'Microsoft',
-  // slackAuthButton: 'Slack',
-  // appleAuthButton: 'Apple',
+  loadingText: 'Loading...',
 };
