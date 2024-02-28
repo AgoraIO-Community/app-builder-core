a5c90a2e6f8dcc1698b33d80e6f2d518068624b6
Supriya Adep <supriya@agora.io>
2024-02-28 18:57:21 +0530

    fix code

--- template/src/utils/useIsRecordingBot.tsx
+++ template/src/utils/useIsRecordingBot.tsx
@@ -1,8 +1,8 @@
 import {useSearchParams} from './useSearchParams';
 
 export function useIsRecordingBot() {
-  // const isRecordingBot = useSearchParams().get('bot');
-  const isRecordingBot = true;
+  const isRecordingBot = useSearchParams().get('bot');
+  // const isRecordingBot = true;
   const recordingBotToken = useSearchParams().get('token');
   const recordingBotName = useSearchParams().get('user_name');
   const isRecordingBotRoute = isRecordingBot && recordingBotToken;
