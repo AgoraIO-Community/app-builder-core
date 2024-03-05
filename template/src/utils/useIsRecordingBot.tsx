ed76225579a2f8854cd1f633ff128cacd2992dec
Supriya Adep <supriya@agora.io>
2024-03-05 18:44:26 +0530

    fix the url check

--- template/src/utils/useIsRecordingBot.tsx
+++ template/src/utils/useIsRecordingBot.tsx
@@ -7,17 +7,24 @@ interface RecordingBotUIConfig {
   stt: boolean;
 }
 
+const regexPattern = new RegExp('true');
+
 export function useIsRecordingBot() {
   const isRecordingBot = useSearchParams().get('bot');
   const recordingBotToken = useSearchParams().get('token');
   const recordingBotName = useSearchParams().get('user_name');
   const isRecordingBotRoute = isRecordingBot && recordingBotToken;
 
+  const chatParam = useSearchParams().get('chat');
+  const topBarParam = useSearchParams().get('topBar');
+  const bottomBarParam = useSearchParams().get('bottomBar');
+  const sttParam = useSearchParams().get('stt');
+
   const recordingBotUIConfig: RecordingBotUIConfig = {
-    chat: Boolean(useSearchParams().get('chat')) || true,
-    topBar: Boolean(useSearchParams().get('topBar')) || true,
-    bottomBar: Boolean(useSearchParams().get('bottomBar')) || true,
-    stt: Boolean(useSearchParams().get('stt')) || true,
+    chat: chatParam ? regexPattern.test(chatParam) : true,
+    topBar: topBarParam ? regexPattern.test(topBarParam) : true,
+    bottomBar: bottomBarParam ? regexPattern.test(bottomBarParam) : true,
+    stt: sttParam ? regexPattern.test(sttParam) : true,
   };
 
   console.log('supriya recordingBotUIConfig', recordingBotUIConfig);
