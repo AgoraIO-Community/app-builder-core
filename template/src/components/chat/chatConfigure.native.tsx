dafc561628fe0c8673b732f5c09a22c4235770c1
Bhupendra Negi <bhupendra1011@gmail.com>
2024-03-13 19:38:01 +0530

    adding native agoar chat & init chat user

--- template/src/components/chat/chatConfigure.native.tsx
+++ template/src/components/chat/chatConfigure.native.tsx
@@ -1,5 +1,8 @@
 import {createHook} from 'customization-implementation';
-import {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
+import {useRoomInfo} from '../room-info/useRoomInfo';
+import {useContent} from 'customization-api';
+import {ChatClient, ChatOptions} from 'react-native-agora-chat';
 
 interface chatConfigureContextInterface {
   open: boolean;
@@ -14,6 +17,50 @@ export const chatConfigureContext =
 
 const ChatConfigure = ({children}) => {
   const [open, setOpen] = useState(false);
+  const {data} = useRoomInfo();
+  const connRef = React.useRef(null);
+  const {defaultContent} = useContent();
+  const defaultContentRef = React.useRef(defaultContent);
+  const chatClient = ChatClient.getInstance();
+
+  const localUid = data?.uid.toString();
+  const agoraToken = data?.chat.user_token;
+
+  React.useEffect(() => {
+    defaultContentRef.current = defaultContent;
+  }, [defaultContent]);
+
+  useEffect(() => {
+    const initializeChatSDK = async () => {
+      const CHAT_APP_KEY = `${$config.CHAT_ORG_NAME}#${$config.CHAT_APP_NAME}`;
+      const chatOptions = new ChatOptions({
+        appKey: CHAT_APP_KEY,
+      });
+
+      try {
+        // initialize native client
+        await chatClient.init(chatOptions);
+        console.warn('chat sdk: init success');
+
+        // log in user to agoar chat
+        try {
+          console.warn('localUId - agoraTopken', localUid, agoraToken);
+          await chatClient.loginWithAgoraToken(localUid, agoraToken);
+          console.warn('chat sdk: login success');
+        } catch (error) {
+          console.warn(
+            'chat sdk: login failed 1',
+            JSON.stringify(error, null, 2),
+          );
+        }
+      } catch (error) {
+        console.warn('chat sdk: init error', error);
+      }
+    };
+
+    initializeChatSDK();
+  }, []);
+
   return (
     <chatConfigureContext.Provider value={{open, setOpen}}>
       {children}
