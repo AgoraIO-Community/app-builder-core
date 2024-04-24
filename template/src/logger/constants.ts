5c5a1b0b77d3ad1a69c82573aa7a2f146fbb9680
Supriya Adep <supriya@agora.io>
2024-04-24 15:32:19 +0530

    change config for now

--- template/src/logger/constants.ts
+++ template/src/logger/constants.ts
@@ -2,8 +2,8 @@ const CONFERENCING_APP_ID = 'b8c2ef0f986541a8992451c07d30fb4b';
 
 // Send logs to configured transport-> Datadog (used for internal app - www.confercning.agora.io)
 // Use the app id strict check to keep this true
-export const ENABLE_AGORA_LOGGER_TRANSPORT =
-  $config.LOG_ENABLED && $config.APP_ID === CONFERENCING_APP_ID && true;
+export const ENABLE_AGORA_LOGGER_TRANSPORT = $config.LOG_ENABLED && true;
+// $config.LOG_ENABLED && $config.APP_ID === CONFERENCING_APP_ID && true;
 
 // Send logs to configured transport-> axiom (used for customer deployed apps)
 export const ENABLE_CUSTOMER_LOGGER_TRANSPORT = $config.LOG_ENABLED && false;
