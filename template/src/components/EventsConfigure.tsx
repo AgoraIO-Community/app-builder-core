41613800225ce65f159f886b5f306a86ae3a31af
Supriya Adep <supriya@agora.io>
2024-04-30 18:59:28 +0530

    remove events from events file

--- template/src/components/EventsConfigure.tsx
+++ template/src/components/EventsConfigure.tsx
@@ -276,9 +276,7 @@ const EventsConfigure: React.FC<Props> = props => {
     //     });
     //   }
     // });
-    events.on('RECORDING_BOT_JOINED', async data => {
-      console.log('Recording-bot: joined custom event received:', data);
-    });
+
     events.on(controlMessageEnum.muteVideo, async ({payload, sender}) => {
       Toast.show({
         leadingIconName: 'video-off',
@@ -790,7 +788,6 @@ const EventsConfigure: React.FC<Props> = props => {
       events.off(EventNames.BOARD_COLOR_CHANGED);
       events.off(EventNames.STT_ACTIVE);
       events.off(EventNames.STT_LANGUAGE);
-      events.off('RECORDING_BOT_JOINED');
     };
   }, []);
 
