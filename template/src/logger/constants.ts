3e5edbda77606e5d023fb81b557056ca63923bb0
Supriya Adep <supriya@agora.io>
2024-05-13 13:53:57 +0530

    disable customer logs

--- template/src/logger/constants.ts
+++ template/src/logger/constants.ts
@@ -10,8 +10,8 @@ export const ENABLE_AGORA_LOGGER_TRANSPORT =
 // $config.LOG_ENABLED && $config.APP_ID === CONFERENCING_APP_ID && true;
 
 // Send logs to configured transport-> axiom (used for customer deployed apps)
-export const ENABLE_CUSTOMER_LOGGER_TRANSPORT =
-  $config.LOG_ENABLED && !isInternalApp;
+export const ENABLE_CUSTOMER_LOGGER_TRANSPORT = false;
+// $config.LOG_ENABLED && !isInternalApp;
 
 // Print logs to browser console window - true in dev mode
 export const ENABLE_BROWSER_CONSOLE_LOGS = $config.LOG_ENABLED;
