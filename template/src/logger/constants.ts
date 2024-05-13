dbcf86bb709ebf4a5fbf821cfb240064514f2ab2
Bhupendra Negi <bhupendra1011@gmail.com>
2024-05-13 11:28:52 +0530

    disabled logging

--- template/src/logger/constants.ts
+++ template/src/logger/constants.ts
@@ -5,14 +5,15 @@ const isInternalApp =
   $config.PROJECT_ID === CONFERENCING_PROJECT_ID;
 // Send logs to configured transport-> Datadog (used for internal app - www.confercning.agora.io)
 // Use the app id strict check to keep this true
-export const ENABLE_AGORA_LOGGER_TRANSPORT =
-  $config.LOG_ENABLED && isInternalApp;
+export const ENABLE_AGORA_LOGGER_TRANSPORT = false;
+//  $config.LOG_ENABLED && isInternalApp;
 // $config.LOG_ENABLED && $config.APP_ID === CONFERENCING_APP_ID && true;
 
 // Send logs to configured transport-> axiom (used for customer deployed apps)
-export const ENABLE_CUSTOMER_LOGGER_TRANSPORT =
-  $config.LOG_ENABLED && !isInternalApp;
+export const ENABLE_CUSTOMER_LOGGER_TRANSPORT = false;
+// $config.LOG_ENABLED && !isInternalApp;
 
 // Print logs to browser console window - true in dev mode
-export const ENABLE_BROWSER_CONSOLE_LOGS = $config.LOG_ENABLED;
+export const ENABLE_BROWSER_CONSOLE_LOGS = false;
+//$config.LOG_ENABLED;
 //   && process.env.NODE_ENV === 'development';
