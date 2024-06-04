e1b519de2bf5ae6a8caacbc2c339abf3a37cb65c
Supriya Adep <supriya@agora.io>
2024-06-04 12:28:19 -0400

    redo the state for private message badge count

--- template/src/subComponents/chat/ChatParticipants.tsx
+++ template/src/subComponents/chat/ChatParticipants.tsx
@@ -250,7 +250,7 @@ const style = StyleSheet.create({
     flexShrink: 1,
   },
   chatNotificationPrivate: {
-    backgroundColor: $config.SEMANTIC_ERROR,
+    backgroundColor: $config.SEMANTIC_NEUTRAL,
     borderRadius: 8,
     alignSelf: 'center',
   },
