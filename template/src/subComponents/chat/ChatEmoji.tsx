15aee505aefde6133236d2511285456413cfba36
Bhupendra Negi <bhupendra1011@gmail.com>
2024-06-10 18:36:40 +0530

    add native emoji

--- template/src/subComponents/chat/ChatEmoji.tsx
+++ template/src/subComponents/chat/ChatEmoji.tsx
@@ -90,6 +90,7 @@ export const ChatEmojiPicker: React.FC = () => {
         previewConfig={{showPreview: false}}
         height={370}
         autoFocusSearch={false}
+        emojiStyle={EmojiStyle.NATIVE}
       />
       <View
         style={{
