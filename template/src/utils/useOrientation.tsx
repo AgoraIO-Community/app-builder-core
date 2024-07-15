62bebd42eaadbe6f11f13b6619aeea8d4dbc57de
Supriya Adep <supriya@agora.io>
2024-07-15 16:46:25 +0530

    block ui on landscape mode

---
+++ template/src/utils/useOrientation.tsx
@@ -0,0 +1,27 @@
+import {useState, useLayoutEffect} from 'react';
+import {Dimensions} from 'react-native';
+
+const isPortrait = () => {
+  const dim = Dimensions.get('window');
+  return dim.height >= dim.width;
+};
+
+/**
+ * A React Hook which updates when the orientation changes
+ * @returns whether the user is in 'PORTRAIT' or 'LANDSCAPE'
+ */
+export function useOrientation(): 'PORTRAIT' | 'LANDSCAPE' {
+  // State to hold the connection status
+  const [orientation, setOrientation] = useState<'PORTRAIT' | 'LANDSCAPE'>(
+    isPortrait() ? 'PORTRAIT' : 'LANDSCAPE',
+  );
+
+  useLayoutEffect(() => {
+    const subscription = Dimensions.addEventListener('change', () => {
+      setOrientation(isPortrait() ? 'PORTRAIT' : 'LANDSCAPE');
+    });
+
+    return () => subscription?.remove();
+  }, []);
+  return orientation;
+}
