849507ac48b43a9d9540023285903ec05c673b82
Hariharan G <ghariharanit@gmail.com>
2024-07-24 20:31:03 +0530

    Update constants.ts

--- template/src/logger/constants.ts
+++ template/src/logger/constants.ts
@@ -10,9 +10,7 @@ export const ENABLE_AGORA_LOGGER_TRANSPORT =
 // $config.LOG_ENABLED && $config.APP_ID === CONFERENCING_APP_ID && true;
 
 // Send logs to configured transport-> axiom (used for customer deployed apps)
-export const ENABLE_CUSTOMER_LOGGER_TRANSPORT =
-  $config.LOG_ENABLED && !isInternalApp;
+export const ENABLE_CUSTOMER_LOGGER_TRANSPORT = false
 
 // Print logs to browser console window - true in dev mode
-export const ENABLE_BROWSER_CONSOLE_LOGS =
-  $config.LOG_ENABLED && process.env.NODE_ENV === 'development';
+export const ENABLE_BROWSER_CONSOLE_LOGS = false
