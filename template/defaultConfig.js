51a164163a0d90f5ed7151b05fcb8e9a27881731
Bhupendra Negi <bhupendra1011@gmail.com>
2024-08-06 12:54:51 +0530

    4.0.26-beta-5 changes

--- template/defaultConfig.js
+++ template/defaultConfig.js
@@ -76,8 +76,8 @@ const DefaultConfig = {
   CHAT_ORG_NAME: '',
   CHAT_APP_NAME: '',
   CHAT_URL: '',
-  CLI_VERSION: '3.0.26-beta-4',
-  CORE_VERSION: '4.0.26-beta-4',
+  CLI_VERSION: '3.0.26-beta-5',
+  CORE_VERSION: '4.0.26-beta-5',
   DISABLE_LANDSCAPE_MODE: false,
 };
 
