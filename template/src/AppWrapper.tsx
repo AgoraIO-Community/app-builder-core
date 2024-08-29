d5a50b7e95db61e859704b86dd56138b9d3914e7
Supriya Adep <supriya@agora.io>
2024-08-29 13:33:59 +0530

    set flag

--- template/src/AppWrapper.tsx
+++ template/src/AppWrapper.tsx
@@ -86,7 +86,7 @@ const AppWrapper = (props: AppWrapperProps) => {
           // @ts-ignore textAlign not supported by TS definitions but is applied to web regardless
           style={[{flex: 1}, Platform.select({web: {textAlign: 'left'}})]}>
           <StatusBar hidden={true} />
-          {$config.DISABLE_LANDSCAPE_MODE && <BlockUI />}
+          {true && <BlockUI />}
           <StorageProvider>
             <LanguageProvider>
               <GraphQLProvider>
