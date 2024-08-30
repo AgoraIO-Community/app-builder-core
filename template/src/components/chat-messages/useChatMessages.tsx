5781e3a678abf08f96911e2fb5e2c4fcb356fa4b
Bhupendra Negi <bhupendra1011@gmail.com>
2024-08-30 10:57:51 +0530

    fixed type

--- template/src/components/chat-messages/useChatMessages.tsx
+++ template/src/components/chat-messages/useChatMessages.tsx
@@ -111,10 +111,10 @@ export interface ChatOption {
   msg?: string;
   file?: object;
   ext?: {
-    file_length: number;
-    file_ext: string;
-    file_name: string;
-    file_url: string;
+    file_length?: number;
+    file_ext?: string;
+    file_name?: string;
+    file_url?: string;
     from_platform?: string;
     channel?: string;
   };
