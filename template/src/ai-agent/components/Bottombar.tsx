7783535efdfc0dab5732117308e1e2841746e65a
HariharanIT <ghariharanit@gmail.com>
2025-02-20 15:55:12 +0530

    Added modal to edit the prompt

--- template/src/ai-agent/components/Bottombar.tsx
+++ template/src/ai-agent/components/Bottombar.tsx
@@ -52,8 +52,7 @@ export const CustomSettingButton = () => {
   }
   return (
     <ToolbarItem>
-      {' '}
-      <IconButton {...iconButtonProps} />{' '}
+      <IconButton {...iconButtonProps} />
     </ToolbarItem>
   );
 };
