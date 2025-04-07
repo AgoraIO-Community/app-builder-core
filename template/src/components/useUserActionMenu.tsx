6d1f37312814a569c4a98722e7412e6fe270ce44
Bhupendra Negi <bhupendra1011@gmail.com>
2025-04-07 13:02:59 +0530

    added pin for everyone util

--- template/src/components/useUserActionMenu.tsx
+++ template/src/components/useUserActionMenu.tsx
@@ -1,16 +1,30 @@
 import React, {createContext, useContext, useState, useEffect} from 'react';
-import {UserActionMenuItemsConfig} from 'customization-api';
+import {
+  UidType,
+  UserActionMenuItemsConfig,
+  DispatchContext,
+  useLayout,
+} from 'customization-api';
+import {
+  DefaultLayouts,
+  getGridLayoutName,
+  getPinnedLayoutName,
+} from '../pages/video-call/DefaultLayouts';
 
 interface UserActionMenuContextType {
   userActionMenuItems: UserActionMenuItemsConfig;
   updateUserActionMenuItems: React.Dispatch<
     React.SetStateAction<UserActionMenuItemsConfig>
   >;
+  pinForEveryone: (uid: UidType) => void;
+  unPinForEveryone: () => void;
 }
 
 const UserActionMenuContext = createContext<UserActionMenuContextType>({
   userActionMenuItems: {},
   updateUserActionMenuItems: () => {},
+  pinForEveryone: () => {},
+  unPinForEveryone: () => {},
 });
 
 export const UserActionMenuProvider: React.FC<{children: React.ReactNode}> = ({
@@ -18,10 +32,33 @@ export const UserActionMenuProvider: React.FC<{children: React.ReactNode}> = ({
 }) => {
   const [userActionMenuItems, updateUserActionMenuItems] =
     useState<UserActionMenuItemsConfig>({});
+  const {dispatch} = useContext(DispatchContext);
+  const {setLayout} = useLayout();
+
+  const pinForEveryone = (uid: UidType) => {
+    dispatch({
+      type: 'UserPin',
+      value: [uid],
+    });
+    setLayout(getPinnedLayoutName());
+  };
+
+  const unPinForEveryone = () => {
+    dispatch({
+      type: 'UserPin',
+      value: [0],
+    });
+    setLayout(getGridLayoutName());
+  };
 
   return (
     <UserActionMenuContext.Provider
-      value={{userActionMenuItems, updateUserActionMenuItems}}>
+      value={{
+        userActionMenuItems,
+        updateUserActionMenuItems,
+        pinForEveryone,
+        unPinForEveryone,
+      }}>
       {children}
     </UserActionMenuContext.Provider>
   );
