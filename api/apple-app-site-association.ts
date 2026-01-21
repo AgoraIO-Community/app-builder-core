de6051eb3d1d1eeeba84ca4663cd803fbaeec550
Supriya Adep <supriya33adep@gmail.com>
2026-01-21 17:04:33 +0530

    add apple site for deep link

---
+++ api/apple-app-site-association.ts
@@ -0,0 +1,14 @@
+export default function handler(_req: any, res: any) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "TEAMID.org.reactjs.native.example.HelloWorld--one",
+          paths: ["/[0-9a-fA-F-]{36}"],
+        },
+      ],
+    },
+  });
+}
