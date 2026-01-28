518d5af9ed05c49bd8da974ab51d4591f7ad73f5
Supriya Adep <supriya33adep@gmail.com>
2026-01-28 11:28:26 +0530

    add qa bundleids

--- api/apple-app-site-association.ts
+++ api/apple-app-site-association.ts
@@ -5,7 +5,11 @@ export default function handler(_req: any, res: any) {
       apps: [],
       details: [
         {
-          appID: "58D77G87LU.io.agora.conferencing.supriya.helloworld",
+          appID: "48TB6ZZL5S.io.agora.app.builder.conferencing",
+          paths: ["*", "/"],
+        },
+        {
+          appID: "34FKK829T7.org.reactjs.native.example.test.Helloworld",
           paths: ["*", "/"],
         },
       ],
