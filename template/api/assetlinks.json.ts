156b9bc57a8354d92dc35c890747fa148bf91cb7
Supriya Adep <supriya33adep@gmail.com>
2026-01-28 12:32:18 +0530

    android asset link

--- template/api/assetlinks.json.ts
+++ template/api/assetlinks.json.ts
@@ -20,7 +20,7 @@ export default function handler(_req: any, res: any) {
         namespace: 'android_app',
         package_name: 'com.helloworld',
         sha256_cert_fingerprints: [
-          'YOUR_SHA256_FINGERPRINT_HERE', // Replace with actual SHA256 fingerprint
+          'FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C',
         ],
       },
     },
